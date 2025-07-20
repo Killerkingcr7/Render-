55154db2c2a83e647f3eef9563a3d97c341661c4
Killerkingcr7 <166912453+Killerkingcr7@users.noreply.github.com>
2025-07-20 14:39:29 +0300

    Update site-config.ts

--- src/utils/site-config.ts
+++ src/utils/site-config.ts
@@ -1,4 +1,4 @@
-export const website_domain = 'tickshark.top';
-export const website_name = 'Tickshark';
+export const website_domain = 'waited.netlify.app';
+export const website_name = 'waited';
 export const default_title = website_name;
 export const TRACKING_STATUS_KEY = 'tracking_status';
